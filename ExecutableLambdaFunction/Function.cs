1f3a28255ed8f8bfbb76013042d41da76f66d8e6
Norm Johanson <normj@amazon.com>
2024-10-23 23:33:28 -0700

    Add Redis Cache to sample

--- ExecutableLambdaFunction/Function.cs
+++ ExecutableLambdaFunction/Function.cs
@@ -1,12 +1,37 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+IConfiguration configuration = new ConfigurationBuilder()
+                                        .AddEnvironmentVariables()
+                                        .Build();
+var redis = ConnectionMultiplexer.Connect(configuration.GetConnectionString("cache")!);
+var redisDatabase = redis.GetDatabase();
+
+
 
 // The function handler that will be called for each Lambda event
 var ToUpper = (string input, ILambdaContext context) =>
 {
-    Console.WriteLine("Input: " + input);
-    return input.ToUpper();
+    context.Logger.LogInformation("Input: " + input);
+    string? upper = null;
+    var cacheResult = redisDatabase.StringGet(new RedisKey(input));
+    if (!cacheResult.HasValue)
+    {
+        context.Logger.LogInformation("Cache miss");
+        upper = input.ToUpper();
+        redisDatabase.StringSet(new RedisKey(input), new RedisValue(upper));
+    }
+    else
+    {
+        upper = cacheResult.ToString();
+        context.Logger.LogInformation("Cache hit");
+    }
+
+    context.Logger.LogInformation("Upper: " + upper);
+    return upper;
 };
 
 // Build the Lambda runtime client passing in the handler to call for each
