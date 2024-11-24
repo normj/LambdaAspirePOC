0e8639b05997f6185957ef781e7b24e85d90e0d2
Norm Johanson <normj@amazon.com>
2024-11-24 15:58:54 -0800

    For https for AppHost launchSettings.json

--- LambdaAspirePOC.AppHost/Program.cs
+++ LambdaAspirePOC.AppHost/Program.cs
@@ -18,15 +18,15 @@ builder.AddAWSLambdaFunction<Projects.ClassLibraryLambdaFunction>("ToLowerLambda
 
 #region Part 2
 // TODO: Get rid of the casts that are due to ClassLibrary and Executable projects returning different types.
-//var rootWebFunction = builder.AddAWSLambdaFunction<Projects.WebApiLambdaFunction>("RootLambdaFunction", handler: "WebApiLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
-//var addFunction = builder.AddAWSLambdaFunction<Projects.WebAddLambdaFunction>("AddLambdaFunction", handler: "WebAddLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
-//var minusFunction = builder.AddAWSLambdaFunction<Projects.WebMinusLambdaFunction>("MinusLambdaFunction", handler: "WebMinusLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
+var rootWebFunction = builder.AddAWSLambdaFunction<Projects.WebApiLambdaFunction>("RootLambdaFunction", handler: "WebApiLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
+var addFunction = builder.AddAWSLambdaFunction<Projects.WebAddLambdaFunction>("AddLambdaFunction", handler: "WebAddLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
+var minusFunction = builder.AddAWSLambdaFunction<Projects.WebMinusLambdaFunction>("MinusLambdaFunction", handler: "WebMinusLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
 
 
-//builder.AddAPIGatewayEmulator("APIGatewayEmulator", APIGatewayType.HttpApi)
-//        .WithReference(rootWebFunction, Method.Get, "/")
-//        .WithReference(addFunction, Method.Get, "/add/{x}/{y}")
-//        .WithReference(minusFunction, Method.Get, "/minus/{x}/{y}");
+builder.AddAPIGatewayEmulator("APIGatewayEmulator", APIGatewayType.HttpApi)
+        .WithReference(rootWebFunction, Method.Get, "/")
+        .WithReference(addFunction, Method.Get, "/add/{x}/{y}")
+        .WithReference(minusFunction, Method.Get, "/minus/{x}/{y}");
 #endregion
 
 
