23bc5b26226b89b2cc5bc9b98b3561bda53de686
Norm Johanson <normj@amazon.com>
2024-12-05 23:30:47 -0800

    Remove code not need to reproduce failure.

--- LambdaAspirePOC.AppHost/Program.cs
+++ LambdaAspirePOC.AppHost/Program.cs
@@ -3,31 +3,6 @@ using Aspire.Hosting.AWS.Lambda;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
-
-var awsConfig = builder.AddAWSSDKConfig()
-                        .WithProfile("default")
-                        .WithRegion(RegionEndpoint.USWest2);
-
-builder.AddAWSLambdaFunction<Projects.ExecutableLambdaFunction>("ToUpperLambdaFunction", handler: "ExecutableLambdaFunction")
-        .WithReference(cache)
-        .WithReference(awsConfig);
-
-builder.AddAWSLambdaFunction<Projects.ClassLibraryLambdaFunction>("ToLowerLambdaFunction", handler: "ClassLibraryLambdaFunction::ClassLibraryLambdaFunction.Function::FunctionHandler")
-        .WithReference(awsConfig);
-
-#region Part 2
-// TODO: Get rid of the casts that are due to ClassLibrary and Executable projects returning different types.
-var rootWebFunction = builder.AddAWSLambdaFunction<Projects.WebApiLambdaFunction>("RootLambdaFunction", handler: "WebApiLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
-var addFunction = builder.AddAWSLambdaFunction<Projects.WebAddLambdaFunction>("AddLambdaFunction", handler: "WebAddLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
-var minusFunction = builder.AddAWSLambdaFunction<Projects.WebMinusLambdaFunction>("MinusLambdaFunction", handler: "WebMinusLambdaFunction") as IResourceBuilder<LambdaProjectResource>;
-
-
-builder.AddAPIGatewayEmulator("APIGatewayEmulator", APIGatewayType.HttpApi)
-        .WithReference(rootWebFunction, Method.Get, "/")
-        .WithReference(addFunction, Method.Get, "/add/{x}/{y}")
-        .WithReference(minusFunction, Method.Get, "/minus/{x}/{y}");
-#endregion
-
+builder.AddAWSLambdaFunction<Projects.ClassLibraryLambdaFunction>("ToLowerLambdaFunction", handler: "ClassLibraryLambdaFunction::ClassLibraryLambdaFunction.Function::FunctionHandler");
 
 builder.Build().Run();
